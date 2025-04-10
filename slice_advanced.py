de79cf93c04509f4dfff8d3e3207c67362cd2856
bauti05 <bperez@mail.austral.edu.ar>
2025-04-09 21:03:48 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,3 +3,8 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    
+
+       texto=input("Escriba su oración:") 
+       print(texto[4: :2])
+  
