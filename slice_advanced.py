1d532d932dd2d8917149a674fbd1c48301e1b624
bauti05 <bperez@mail.austral.edu.ar>
2025-04-10 11:40:36 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,10 +1,12 @@
 def slice_advanced():
+    
+    texto=input("Escriba su oración:") 
+    print(texto[4: :2])
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
     
 
-       texto=input("Escriba su oración:") 
-       print(texto[4: :2])
+
   
